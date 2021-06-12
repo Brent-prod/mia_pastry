eae04e69372c4c7cc0bb70954a7a66f269ceea62
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-06-12 13:55:45 +1000

    first commit

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-     
+require 'sinatra'     
 require 'sinatra/reloader' if development?
 require 'pg'
 
@@ -6,11 +6,11 @@ enable :sessions
 
 require_relative 'db/db'
 # require_relative 'models/food'
-require_relative 'models/user'
-require_relative 'helpers/sessions_helper'
+# require_relative 'models/user'
+# require_relative 'helpers/sessions_helper'
 # require_relative 'controllers/food_controller'
-require_relative 'controllers/users_controller'
-require_relative 'controllers/sessions_controller'
+# require_relative 'controllers/users_controller'
+# require_relative 'controllers/sessions_controller'
 
 
 get '/' do
