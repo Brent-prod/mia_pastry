4027ddbcdd60eb13a8bd82ad81f87237aef11202
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-06-12 12:39:10 +1000

    first commit

---
+++ DB/db.rb
@@ -0,0 +1,18 @@
+
+def run_sql(sql_query, params = [])
+# Take a params 2nd argument
+# params = [] -> set default value of the argument to []
+   
+   connection = PG.connect(ENV['DATABASE_URL'] || {dbname: 'mia_db'})
+
+   #results = connection.exec(sql_query)
+# First Arg => Unique Name for prepared statement.
+# Second Arg => SQL Query
+
+   connection.prepare("statement_name", sql_query)
+   results = connection.exec_prepared("statement_name", params)
+   
+   connection.close
+ 
+   return results
+ end
---
+++ main.rb
@@ -0,0 +1,23 @@
+     
+require 'sinatra/reloader' if development?
+require 'pg'
+
+enable :sessions
+
+require_relative 'db/db'
+# require_relative 'models/food'
+require_relative 'models/user'
+require_relative 'helpers/sessions_helper'
+# require_relative 'controllers/food_controller'
+require_relative 'controllers/users_controller'
+require_relative 'controllers/sessions_controller'
+
+
+get '/' do
+  erb :index
+end
+
+
+
+
+
