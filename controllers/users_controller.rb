147842ec8f0ba79ea027e07322ae4bc3f570f9e9
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-06-16 20:07:26 +1000

    daily progress

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -3,10 +3,18 @@ get '/signup' do
 end
 
 post '/users' do
-  email = params[:email]
+  email = params[:email] 
   password = params[:password]
+  # these two are receiving info from signup.erb
 
-  create_user(email, password)
+  create_user(email, password) 
+  # it creates a new user
+
+  user = find_user_by_email( email )
+  # finding an user by email to get the "id"
+
+  session[:user_id] = user['id']
+  # assinging the current user id to the session id 
 
   redirect '/cakes'
 end
