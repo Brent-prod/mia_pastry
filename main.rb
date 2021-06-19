dbb0f7efc3680b0c41620caf0043428cfc256a95
Brent <76509483+Brent-prod@users.noreply.github.com>
2021-06-19 11:11:31 +1000

    daily progress

--- main.rb
+++ main.rb
@@ -16,23 +16,3 @@ require_relative 'controllers/sessions_controller'
 get '/' do
   erb :index
 end
-
-# get '/login' do
-#   erb :login
-# end
-
-# get '/signup' do
-#   erb :signup
-# end
-
-# # get '/display' do
-# #   erb :display
-# # end
-
-# get '/create' do
-#   erb :create
-# end
-
-# get '/edit' do
-#   erb :edit
-# end
